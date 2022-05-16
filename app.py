8b31aaca384af36a8c97db29d609b7b2dfdeb4c6
PiuPiPiu <katernd@mail.ru>
2022-05-16 13:43:36 +0300

    add document-service code

--- app.py
+++ app.py
@@ -1,155 +1,105 @@
 ﻿from flask import Flask
 from flask_restful import Api, Resource, reqparse
 import random
+import os
+import re
 
 app = Flask(__name__)
 api = Api(app)
 
-ai_quotes = [
-    {
-        "id": 0,
-        "author": "Kevin Kely",
-        "quote": "The business plans of the next 10,000 startups are easy to forecast: " +
-                 "Take X and add AI."
-    },
-    {
-        "id": 1,
-        "author": "Stephen Hawking",
-        "quote": "The development of full artificial intelligence could " +
-                 "spell the end of the human race… " +
-                 "It would take off on its own, and re-design " +
-                 "itself at an ever increasing rate. " +
-                 "Humans, who are limited by slow biological evolution, " +
-                 "couldn't compete, and would be superseded."
-    },
-    {
-        "id": 2,
-        "author": "Claude Shannon",
-        "quote": "I visualize a time when we will be to robots what " +
-                 "dogs are to humans, " +
-                 "and I’m rooting for the machines."
-    },
-    {
-        "id": 3,
-        "author": "Elon Musk",
-        "quote": "The pace of progress in artificial intelligence " +
-                 "(I’m not referring to narrow AI) " +
-                 "is incredibly fast. Unless you have direct " +
-                 "exposure to groups like Deepmind, " +
-                 "you have no idea how fast — it is growing " +
-                 "at a pace close to exponential. " +
-                 "The risk of something seriously dangerous " +
-                 "happening is in the five-year timeframe." +
-                 "10 years at most."
-    },
-    {
-        "id": 4,
-        "author": "Geoffrey Hinton",
-        "quote": "I have always been convinced that the only way " +
-                 "to get artificial intelligence to work " +
-                 "is to do the computation in a way similar to the human brain. " +
-                 "That is the goal I have been pursuing. We are making progress, " +
-                 "though we still have lots to learn about " +
-                 "how the brain actually works."
-    },
-    {
-        "id": 5,
-        "author": "Pedro Domingos",
-        "quote": "People worry that computers will " +
-                 "get too smart and take over the world, " +
-                 "but the real problem is that they're too stupid " +
-                 "and they've already taken over the world."
-    },
-    {
-        "id": 6,
-        "author": "Alan Turing",
-        "quote": "It seems probable that once the machine thinking " +
-                 "method had started, it would not take long " +
-                 "to outstrip our feeble powers… " +
-                 "They would be able to converse " +
-                 "with each other to sharpen their wits. " +
-                 "At some stage therefore, we should " +
-                 "have to expect the machines to take control."
-    },
-    {
-        "id": 7,
-        "author": "Ray Kurzweil",
-        "quote": "Artificial intelligence will reach " +
-                 "human levels by around 2029. " +
-                 "Follow that out further to, say, 2045, " +
-                 "we will have multiplied the intelligence, " +
-                 "the human biological machine intelligence " +
-                 "of our civilization a billion-fold."
-    },
-    {
-        "id": 8,
-        "author": "Sebastian Thrun",
-        "quote": "Nobody phrases it this way, but I think " +
-                 "that artificial intelligence " +
-                 "is almost a humanities discipline. It's really an attempt " +
-                 "to understand human intelligence and human cognition."
-    },
-    {
-        "id": 9,
-        "author": "Andrew Ng",
-        "quote": "We're making this analogy that AI is the new electricity." +
-                 "Electricity transformed industries: agriculture, " +
-                 "transportation, communication, manufacturing."
-    }
-]
+last_files = []
 
 
-class Quote(Resource):
+def files_enum(folder):
+    files = os.listdir(folder)
+    output = {'files': []}
+    rules = ['[0-9][0-9]_[0-9][0-9]_[0-9][0-9][0-9][0-9]'] # Правила для определения дат в названии
+    id = 1
+    for file in files: # Перебор файлов
+        for rule in rules: # Перебор правил
+            date = '';
+
+            if re.search(rule, file):
+                date = str(re.search(rule, file).group(0))
+                date = date[6:] + '-' + date[3:5] + '-' + date[:2] # Форматирование даты из дд_мм_гггг в гггг-мм-дд
+
+            obj = {
+                'name': file,
+                'date': date,
+                'visible': True,
+                'id': id
+            }
+            output['files'].append(obj)
+            id = id + 1
+
+    return output['files']
+
+# def send(files):
+#     print(files)
+
+class Documents(Resource):
     def get(self, id=0):
         if id == 0:
-            return random.choice(ai_quotes), 200
-        for quote in ai_quotes:
-            if(quote["id"] == id):
-                return quote, 200
-        return "Quote not found", 404
+            return current_files, 200
+        for file in current_files:
+            return file, 200
+        return "File not found", 404
 
     def post(self, id):
       parser = reqparse.RequestParser()
-      parser.add_argument("author")
-      parser.add_argument("quote")
+      parser.add_argument("name")
+      parser.add_argument("date")
+      parser.add_argument("visible")
       params = parser.parse_args()
-      for quote in ai_quotes:
-          if(id == quote["id"]):
-              return f"Quote with id {id} already exists", 400
-      quote = {
+      for file in last_files:
+          if(id == file["id"]):
+              return f"File with id {id} already exists", 400
+      file = {
           "id": int(id),
-          "author": params["author"],
-          "quote": params["quote"]
+          "name": params["name"],
+          "date": params["date"],
+          "visible": params["visible"]
       }
-      ai_quotes.append(quote)
-      return quote, 201
+      last_files.append(file)
+      return file, 201
 
     def put(self, id):
       parser = reqparse.RequestParser()
-      parser.add_argument("author")
-      parser.add_argument("quote")
+      parser.add_argument("name")
+      parser.add_argument("date")
+      parser.add_argument("visible")
       params = parser.parse_args()
-      for quote in ai_quotes:
-          if(id == quote["id"]):
-              quote["author"] = params["author"]
-              quote["quote"] = params["quote"]
-              return quote, 200
+      for file in last_files:
+          if(id == file["id"]):
+              file["name"] = params["name"]
+              file["date"] = params["date"]
+              file["visible"] = params["visible"]
+              return file, 200
 
-      quote = {
+      file = {
           "id": id,
-          "author": params["author"],
-          "quote": params["quote"]
+          "name": params["name"],
+          "date": params["date"],
+          "visible": params["visible"]
       }
 
-      ai_quotes.append(quote)
-      return quote, 201
-
     def delete(self, id):
-      global ai_quotes
-      ai_quotes = [qoute for qoute in ai_quotes if qoute["id"] != id]
-      return f"Quote with id {id} is deleted.", 200
+        global last_files
+        last_files = [file for file in last_files if file["id"] != id]
+        return f"File with id {id} is deleted.", 200
 
 
-api.add_resource(Quote, "/ai-quotes", "/ai-quotes/", "/ai-quotes/<int:id>")
-if __name__ == '__main__':
+api.add_resource(Documents, "/document-service", "/document-service/", "/document-service/<int:id>")
+
+while True:
+    current_files = files_enum(r'C:\Users\Ekaterina\Desktop\sfedu-documents')
+    # if last_files != current_files:
+    #     send(current_files)
+    last_files = current_files
     app.run(debug=True)
+
+
+
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
