89471a5170a88592396a02df3dead41dfc2bb16e
PiuPiPiu <katernd@mail.ru>
2022-05-22 23:12:12 +0300

    Fix variable again

--- app.py
+++ app.py
@@ -113,7 +113,7 @@ class Documents(Resource):
 api.add_resource(Documents, "/document-service", "/document-service/", "/document-service/<int:id>")
 
 if __name__ == '__main__':
-    current_files = files_enum(r'C:\Users\Ekaterina\Desktop\sfedu-documents')
+    current_files = files_enum(r'C:\Users\Ekaterina\Desktop\university-documents')
     # if last_files != current_files:
     #     send(current_files)
     last_files = current_files
